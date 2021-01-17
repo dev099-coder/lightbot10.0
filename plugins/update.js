d9cf7ae71d8e42e75fdee71a2c52c0f627efa583
dev099-coder <75147290+dev099-coder@users.noreply.github.com>
2021-01-17 20:13:39 -0300

    Update update.js

--- plugins/update.js
+++ plugins/update.js
@@ -2,6 +2,7 @@ let { execSync } = require('child_process')
 let handler = async (m, { conn, text }) => {
   if (global.conn.user.jid == conn.user.jid) {
     let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
+    require('fs').readdirSync('plugins').map(v=>global.reload('', v))
     conn.reply(m.chat, stdout.toString(), m)
   }
 }
@@ -21,4 +22,3 @@ handler.fail = null
 handler.exp = 0
 
 module.exports = handler
-
